5554c3f6c4601c91f82ede31e740f9c3b0bdf93d
David Demerchyan <davidemer11@yandex.ru>
2024-11-24 20:51:01 +0300

    Add default router

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/DKDemerchyan/todo-list
 go 1.23.3
 
 require (
+	github.com/go-chi/chi/v5 v5.1.0
 	github.com/jmoiron/sqlx v1.4.0
 	github.com/mattn/go-sqlite3 v1.14.24
 	github.com/stretchr/testify v1.10.0
--- main.go
+++ main.go
@@ -1,4 +1,17 @@
 package main
 
+import (
+	"github.com/go-chi/chi/v5"
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
+	router := chi.NewRouter()
+
+	port := os.Getenv("TODO_PORT")
+	if err := http.ListenAndServe(":"+port, router); err != nil {
+		log.Fatal(err)
+	}
 }
