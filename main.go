2af9a9b01733722ed4afbd2aca57501f152404a2
David Demerchyan <davidemer11@yandex.ru>
2024-12-01 13:34:37 +0300

    Add database connection

--- main.go
+++ main.go
@@ -4,20 +4,37 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
+	"github.com/DKDemerchyan/todo-list/database"
 	"github.com/joho/godotenv"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
-	mux := http.NewServeMux()
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
+	appPath, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	dbFile := filepath.Join(appPath, os.Getenv("TODO_DBFILE"))
+
+	db, err := database.ConnectDB(dbFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	mux := http.NewServeMux()
 	webDir := "./web"
 
 	mux.Handle("/", http.FileServer(http.Dir(webDir)))
 
-	err := godotenv.Load()
 	port := ":" + os.Getenv("TODO_PORT")
-	if err = http.ListenAndServe(port, mux); err != nil {
+	if err := http.ListenAndServe(port, mux); err != nil {
 		log.Fatal(err)
 	}
 }
