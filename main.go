29f997a030b8a90d5dd1f6cb9976208cd57ff703
David Demerchyan <davidemer11@yandex.ru>
2024-12-08 22:10:29 +0300

    Separate router setup and use routing inside

--- main.go
+++ main.go
@@ -31,22 +31,31 @@ func main() {
 	store := database.NewTaskStore(db)
 
 	// Routing
-	webDir := "./web"
-	fileServer := http.FileServer(http.Dir(webDir))
-
-	router := chi.NewRouter()
-
-	router.Mount("/", fileServer)
-	router.Get("/api/nextdate", handlers.NextDate)
-	router.Get("/api/task", handlers.GetTask(store))
-	router.Post("/api/task", handlers.CreateTask(store))
-	router.Put("/api/task", handlers.UpdateTask(store))
-	router.Delete("/api/task", handlers.DeleteTask(store))
-	router.Post("/api/task/done", handlers.TaskDone(store))
-	router.Get("/api/tasks", handlers.GetTasks(store))
-
+	router := setupRouter(store)
 	port := ":" + os.Getenv("TODO_PORT")
 	if err := http.ListenAndServe(port, router); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func setupRouter(store database.TaskStore) http.Handler {
+	webDir := "./web"
+	fileServer := http.FileServer(http.Dir(webDir))
+
+	router := chi.NewRouter()
+	router.Mount("/", fileServer)
+
+	router.Route("/api", func(r chi.Router) {
+		r.Get("/nextdate", handlers.NextDate)
+		r.Get("/tasks", handlers.GetTasks(store))
+
+		r.Route("/task", func(rr chi.Router) {
+			rr.Get("/", handlers.GetTask(store))
+			rr.Post("/", handlers.CreateTask(store))
+			rr.Put("/", handlers.UpdateTask(store))
+			rr.Delete("/", handlers.DeleteTask(store))
+			rr.Post("/done", handlers.TaskDone(store))
+		})
+	})
+	return router
 }
